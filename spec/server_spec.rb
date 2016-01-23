3f0c61f0388a376730aa361096344a8e7bb31197
apeckham <apeckham@gmail.com>
2016-01-23 12:11:19 -0800

    fill out more data and tests

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -8,5 +8,8 @@ describe 'server' do
       with_text /Page title from index\.yaml/
     end
     expect(last_response.body).to have_tag('meta[itemprop=url]', with: {content: "http://example.com/products/helloworld"})
+    expect(last_response.body).to have_tag('p[role=contentinfo]') do
+      with_text /Copyright © \d+ MyStore\./
+    end
   end
 end
