491592f3412731cc5c73d09b6e7dc44f941fc47c
apeckham <apeckham@gmail.com>
2016-01-29 21:56:17 -0800

    reorder

--- helpers.rb
+++ helpers.rb
@@ -1,10 +1,12 @@
 module Helpers
-  def parse_template(path)
-    Liquid::Template.parse(File.read(path))
-  end
-
   def yaml(path)
     YAML.load_file(path)
+  end
+
+  def yaml_merge(*paths)
+    out = {}
+    paths.each { |path| out.deep_merge!(yaml(path)) }
+    out
   end
 
   def theme_path(path)
@@ -15,6 +17,10 @@ module Helpers
     Sass::Engine.new(source, syntax: :scss).render
   end
 
+  def parse_template(path)
+    Liquid::Template.parse(File.read(path))
+  end
+
   def render_template(vars, path)
     template = parse_template(theme_path(path))
     template.render!(vars, {strict_variables: true, strict_filters: true})
@@ -23,11 +29,5 @@ module Helpers
   def render_template_in_theme(vars, path)
     html = render_template(vars, path)
     render_template(vars.merge('content_for_layout' => html), 'layout/theme.liquid')
-  end
-
-  def yaml_merge(*paths)
-    out = {}
-    paths.each { |path| out.deep_merge!(yaml(path)) }
-    out
   end
 end
